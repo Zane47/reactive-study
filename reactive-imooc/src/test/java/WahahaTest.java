93933ff3f15b1d04de457f3271f3f87a4bf6894f
Zane47 <1228073191@qq.com>
2023-03-29 13:31:54 +0800

    subscription

--- reactive-imooc/src/test/java/WahahaTest.java
+++ reactive-imooc/src/test/java/WahahaTest.java
@@ -13,15 +13,66 @@ import java.time.Duration;
 public class WahahaTest {
 
 
+    @Test
+    public void subscribe1() {
+        Flux.range(1, 100)
+                .log()
+                .limitRate(10)
+                .subscribe(System.out::println);
+    }
+
+    @Test
+    public void subscribe2() {
+        Flux.range(1, 100).log()
+                .map(i -> {
+                    if (i <= 3) {
+                        return i * 2;
+                    }
+                    throw new RuntimeException("test 4");
+                })
+                .subscribe(System.out::println, error -> System.out.println("error: " + error));
+    }
+
+
+    /**
+     * 错误信号和完成信号都是终止事件，并且彼此互斥（你永远不会同时得到这两个信号）
+     */
+    @Test
+    public void subscribe3() {
+        Flux.range(1, 4)
+                .subscribe(System.out::println,
+                        error -> System.err.println("Error " + error),
+                        () -> System.out.println("Done"));
+    }
+
+    /**
+     * 表示我们最多希望从源（实际上会发出10个元素）中接收到 5 个元素。
+     */
+    @Test
+    public void subscribe4() {
+        Flux.range(1, 10)
+                .subscribe(System.out::println,
+                        error -> System.err.println("Error " + error),
+                        () -> System.out.println("Done"),
+                        sub -> sub.request(5));
+    }
+
+
 
     @Test
     public void test() {
         long now = System.currentTimeMillis();
+
+        // Flux.interval()
+
+
+        System.out.println("------------------------------");
+
         Flux.range(1, 100)
-                .log()
                 .limitRate(1)
+                .log()
                 .flatMap(this::times2)
-                .delaySequence(Duration.ofMillis(1))
+                .delayElements(Duration.ofMillis(1000))
                 .subscribe(System.out::println);
         System.out.println(System.currentTimeMillis() - now);
 
